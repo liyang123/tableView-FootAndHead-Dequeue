488dba604a67a46491b0615f39744777b57de45d
liyang2016 <ly@liyangdeimac.local>
2016-03-03 16:30:47 +0800

    Initial Commit

---
+++ TestDequeue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDequeue
+//
+//  Created by ly on 16/3/3.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDequeue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDequeue
+//
+//  Created by ly on 16/3/3.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
